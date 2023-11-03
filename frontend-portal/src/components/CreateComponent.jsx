6dafb6c260ffef0bd847c0076c6578fe4b657590
Monalysa <monalysaklauck@gmail.com>
2023-11-03 13:55:38 -0300

    style: add bootstrap, responsidade and image favicon

--- frontend-portal/src/components/CreateComponent.jsx
+++ frontend-portal/src/components/CreateComponent.jsx
@@ -1,5 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
 import React, {useState} from 'react'
-import postService from '../service/postService';
+import registerService from '../service/registerService';
+import { useNavigate } from "react-router-dom";
+
 
 function CreateComponent(){
     const [name,setName] = useState('');
@@ -8,12 +12,18 @@ function CreateComponent(){
     const [cel,setCel] = useState('');
     const [rg,setRg] = useState('');
     const [cpf,setCpf] = useState('');
-    const [fileDoc,setFileDoc] = useState('');
     const [address,setAddress] = useState('');
+    const [city,setCity] = useState('');
+    const [state,setState] = useState('');
+    const [zip,setZip] = useState('');
+
+    const [fileDoc,setFileDoc] = useState('');
     const [fileAddress,setFileAddress] = useState('');
     const [fileEmployContract,setFileEmployContract] = useState('');
     const [fileResume,setFileResume] = useState('');
     const [message,setMessage] = useState('');
+
+    const navigate = useNavigate();
 
     const handleSubmit = async(event)=>{
         event.preventDefault();
@@ -28,11 +38,14 @@ function CreateComponent(){
         formData.append('cpf', cpf);
         formData.append('fileDoc', fileDoc);
         formData.append('address', address);
+        formData.append('city', city);
+        formData.append('state', state);
+        formData.append('zip', zip);
         formData.append('fileAddress', fileAddress);
         formData.append('fileEmployContract', fileEmployContract);
         formData.append('fileResume', fileResume);
 
-        const response = await postService.create(formData);
+        const response = await registerService.create(formData);
         if(response.data.success == true){
             setMessage('Funcionario adicionado com sucesso!!!')
 
@@ -48,74 +61,163 @@ function CreateComponent(){
     };
 
     return (
-        <div>
-            <h2> Seu formulario</h2>
-            <form onSubmit={handleSubmit}>
-                <input type="text"
-                    name="name"
-                    placeholder='Seu nome'
-                    onChange={event => setName(event.target.value)}
-                    required/>
-                <br/><br/>
-                <input type="date"
-                    name="date"
-                    onChange={event => setDate(event.target.value)}
-                    required/>
-                <br/><br/>
-                <input type="text"
-                    name="email"
-                    placeholder='coloque seu email'
-                    onChange={event => setEmail(event.target.value)}
-                    required/>
-                <br/><br/>
-                <input type="number"
-                    name="celular"
-                    placeholder='coloque seu numero'
-                    onChange={event => setCel(event.target.value)}
-                    required/>
-                <br/><br/>
-                <input type="number"
-                    name="rg"
-                    placeholder='Coloque seu RG sem ponto'
-                    onChange={event => setRg(event.target.value)}
-                    required/>
-                <br/><br/>
-                <input type="number"
-                    name="cpf"
-                    placeholder='Coloque seu cpf sem ponto'
-                    onChange={event => setCpf(event.target.value)}
-                    required/>
-                <br/><br/>
-                <input type="file"
-                    name="fileDoc"
-                    onChange={event => setFileDoc(event.target.files[0])}
-                    required/>
-                <br/><br/>
-                <input type="text"
-                    name="address"
-                    placeholder='Insira seu endereço'
-                    onChange={event => setAddress(event.target.value)}
-                    required/>
-                <br/><br/>
-                <input type="file"
-                    name="fileAddress"
-                    onChange={event => setFileAddress(event.target.files[0])}
-                    required/>
-                <br/><br/>
-                <input type="file"
-                    name="fileEmployContract"
-                    onChange={event => setFileEmployContract(event.target.files[0])}
-                    required/>
-                <br/><br/>
-                <input type="file"
-                    name="fileResume"
-                    onChange={event => setFileResume(event.target.files[0])}
-                    required/>
-                <br/><br/>
-                <button>Enviar</button>
+        <div className="container p-4">
+            <div className="d-flex p-4 justify-content-center border border-primary" >
+                <div className="mb-auto"> 
+                    <h2> Seu formulario</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className="row">
+                            <div className="form-group col-md-4">
+                                <label for="inputName">Nome e Sobrenome </label>
+                                <input type="name" placeholder='Coloque seu nome completo' className="form-control" id="inputName" onChange={event => setName(event.target.value)} required/>
+                            </div>
+                            <div className="col-md-4">
+                                <label for="inputDate">Data de nascimento </label>
+                                <input type="date" 
+                                placeholder='Coloque seu nome completo' 
+                                className="form-control" 
+                                id="inputName" 
+                                onChange={event => setDate(event.target.value)} 
+                                required/>
+                            </div>
+                            <div className="w-100"></div>
+                            <div className="col-md-4">
+                                <label for="inputEmail">Email</label>
+                                <input type="email" className="form-control" id="inputEmail"
+                                onChange={event => setEmail(event.target.value)}
+                                required/>
+                            </div>                            
+                            <div className="col-md-4">
+                                <label for="inputCel">Telefone/Celular</label>
+                                <input type="number" className="form-control" id="inputCel"
+                                onChange={event => setCel(event.target.value)}
+                                required/>
+                            </div>
+                            <div className="w-100"></div>
+                            <div className="col-md-4">
+                                <label for="inputRg">RG</label>
+                                <input type="text" className="form-control" id="inputRg"
+                                onChange={event => setRg(event.target.value)}
+                                placeholder='xxxxxxxx-x'
+                                required/>
+                            </div>
+                            <div className="col-md-4">
+                                <label for="inputCpf">CPF</label>
+                                <input type="text" className="form-control" id="inputCpf"
+                                onChange={event => setCpf(event.target.value)}
+                                placeholder='xxx.xxx.xxx-xx'
+                                required/>
+                            </div>
+                            <div className="w-100"></div>
+                                                       
+                            <div className=" col-md-6">
+                                <label for="inputAddress">Endereço</label>
+                                <input type="text" className="form-control" id="inputAddress2" 
+                                placeholder="Ex: Rua da contratada numero 01"
+                                onChange={event => setAddress(event.target.value)}
+                                required/>
+                            </div>
 
-            </form>
-            <p>{message}</p>
+                            <div className="col-md-5">
+                                <div className="form-group col-md-3">
+                                    <label for="inputCity">Cidade</label>
+                                    <input type="text" className="form-control" 
+                                    onChange={event => setCity(event.target.value)}
+                                    id="inputCity"/>
+                                </div>
+                            </div> 
+
+                            <div className="w-100"></div>
+                            
+                            <div className="form-group col-md-2">
+                                <label for="inputState">Estado</label>
+                                <select id="inputState" className="form-control" onChange={event => setState(event.target.value)}>
+                                    <option selected>Selecione</option>
+                                    <option>AC</option>
+                                    <option>AL</option>
+                                    <option>AP</option>
+                                    <option>AM</option>
+                                    <option>BA</option>
+                                    <option>CE</option>
+                                    <option>DF</option>
+                                    <option>ES</option>
+                                    <option>GO</option>
+                                    <option>MA</option>
+                                    <option>MT</option>
+                                    <option>MS</option>
+                                    <option>MG</option>
+                                    <option>PA</option>
+                                    <option>PB</option>
+                                    <option>PR</option>
+                                    <option>PE</option>
+                                    <option>PI</option>
+                                    <option>RJ</option>
+                                    <option>RN</option>
+                                    <option>RS</option>
+                                    <option>RO</option>
+                                    <option>RR</option>
+                                    <option>SC</option>
+                                    <option>SP</option>
+                                    <option>TO</option>
+
+                                </select>
+                            </div>
+                            <div className="form-group col-md-2">
+                                <label for="inputZip">CEP</label>
+                                <input type="text" className="form-control"  
+                                onChange={event => setZip(event.target.value)}
+                                id="inputZip"/>
+                            </div>
+                            <div className="w-100"></div>
+
+                        </div>
+                        <div className="row">
+                            <div className="col-md-4">
+                                <label for="fileDoc">Anexo de RG e CPF</label>
+                                <input type="file" className="form-control-file" id="fileDoc"
+                                onChange={event => setFileDoc(event.target.files[0])}
+                                required/>
+                            </div>
+                                    
+                            <div className="col-md-4">
+                                <label for="fileAddress">Comprovante de residência</label>
+                                <input type="file" className="form-control-file" id="fileAddress"
+                                onChange={event => setFileAddress(event.target.files[0])}
+                                required/>
+                            </div>
+                            <div className="w-100"></div>
+                            <div className="col-md-4">
+                                <label for="fileEmployContract">Contrato de Trabalho</label>
+                                <input type="file" className="form-control-file" id="fileEmployContract"
+                                onChange={event => setFileEmployContract(event.target.files[0])}
+                                required/>
+                            </div>
+                            <div className="col-md-4">
+                                <label for="fileResume">Curriculo</label>
+                                <input type="file" className="form-control-file" id="fileResume"
+                                onChange={event => setFileResume(event.target.files[0])}
+                                required/>
+                            </div>
+                            <div className="w-100"></div>
+
+                        </div>
+
+                            
+                        
+                        <button type="submit" className="btn btn-primary mb-2">Enviar</button>
+
+                        
+                        <button classNameNameName="btn btn-secondary btn-sm mr-3" onClick={() => navigate("/show")}>Lista de cadastro</button>
+
+
+                        
+                        
+
+                    </form>
+                    <p>{message}</p>
+                </div>
+                
+            </div>
         </div>
     )
 };
--- frontend-portal/src/components/ShowComponent.jsx
+++ frontend-portal/src/components/ShowComponent.jsx
@@ -1,78 +1,110 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import {useState, useEffect} from 'react';
-import postService from '../service/postService';
+import registerService from '../service/registerService';
 import UpdateModalComponent from "./UpdateModalComponent";
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import {Table, Button} from 'react-bootstrap'
 
 function ShowComponent(){
+    
 
-    const[posts, setPosts] = useState({});
+    const[registers, setRegisters] = useState({});
 
-    const fetchPosts = async()=>{
-        setPosts(await postService.gestPosts());
+    const fetchRegisters = async()=>{
+        try{
+            setRegisters(await registerService.getRegisters());
+        }catch (error) {
+            console.error('Erro ao buscar os registers', error);
+        }
+        
+        
     }
 
-    fetchPosts();
-
     useEffect(()=>{
-        fetchPosts();
+        fetchRegisters();
 
-    },[posts]);
+    },[]);
 
-    const deletePost = async(id, e)=>{
+    const deleteRegister = async(id, e)=>{
+        try {
+            let response =  await registerService.deleteRegister(id);
+            console.log('erro do response', response.data);
+        
+            if(response.data.success === true){
+                    alert(response.data.msg);
+                    document.getElementById(id).parentElement.parentElement.remove();
+            }else{
+                alert(response.data.msg);
+            }
+            
+        } catch (error) {
+            console.error('Erro ao excluir o register', error);
+            
+        }
 
-       let response =  await postService.deletePost(id);
-       if(response.data.success == true){
-            alert(response.data.msg);
-            document.getElementById(id).parentElement.parentElement.remove();
-
-       }else{
-            alert(response.data.msg);
-
-       }
+       
         
     }
 
     return(
-        <div className="App">
+        <div class="App">
+            
             <h2>Lista de cadastro</h2>
             {
-                posts.data != undefined && posts.data.data.length > 0 && (
-                    <table style={{width:'100%'}} border='1'>
-                        <thead>
-                            <th>Nome Completo</th>
-                            <th>Data de nascimento</th>
-                            <th>E-mail</th>
-                            <th>Celular</th>
-                            <th>RG</th>
-                            <th>CPF</th>
-                            <th>PDF RG/CPF</th>
-                            <th>Endereço</th>
-                            <th>Comprovante residencial</th>
-                            <th>Contrato de trabalho</th>
-                            <th>Curriculo atualizado</th>
-                            <th>Delete</th>
-                            <th>Editar</th>
+                
+                registers.data != undefined && registers.data.data.length > 0 && (
+                    <table class="table" border='1'>
+                        <thead class="thead-dark">
+                            <ths cope="col">Nome Completo</ths>
+                            <th scope="col">Data de nascimento</th>
+                            <th scope="col">E-mail</th>
+                            <th scope="col">Celular</th>
+                            <th scope="col">RG</th>
+                            <th scope="col">CPF</th>
+                            <th scope="col">Endereço</th>
+                            <th scope="col">Cidade</th>
+                            <th scope="col">Estado</th>
+                            <th scope="col">CEP</th>
+                            <th scope="col">PDF RG/CPF</th>
+                            <th scope="col">Comprovante residencial</th>
+                            <th scope="col">Contrato de trabalho</th>
+                            <th scope="col">Curriculo atualizado</th>
+                            <th scope="col">Delete</th>
+                            <th scope="col">Editar</th>
                         </thead>
                         <tbody>
                             {
-                                posts.data.data.map(post=>(
+                                registers.data.data.map(register=>(
                                     <tr>
-                                        <td>{post.name}</td>
-                                        <td>{post.date}</td>
-                                        <td>{post.email}</td>
-                                        <td>{post.cel}</td>
-                                        <td>{post.rg}</td>
-                                        <td>{post.cpf}</td>
-                                        <td>{post.fileDoc}</td>
-                                        <td>{post.address}</td>
-                                        <td>{post.fileAddress}</td>
-                                        <td>{post.fileEmployContract}</td>
-                                        <td>{post.fileResume}</td>
+                                        <td>{register.name}</td>
+                                        <td>{register.date}</td>
+                                        <td>{register.email}</td>
+                                        <td>{register.cel}</td>
+                                        <td>{register.rg}</td>
+                                        <td>{register.cpf}</td>                                        
+                                        <td>{register.address}</td>
+                                        <td>{register.city}</td>
+                                        <td>{register.state}</td>
+                                        <td>{register.zip}</td>
+                                        <td style={{width:'10%'}}>{register.fileDoc}</td>
+                                        <td style={{width:'10%'}}>{register.fileAddress}</td>
+                                        <td style={{width:'10%'}}>{register.fileEmployContract}</td>
+                                        <td style={{width:'10%'}}>{register.fileResume}</td>
                                         <td>
-                                            <button id={post._id} onClick={(e)=>deletePost(post._id,e)}>Delete</button>
+                                            <button id={register._id} onClick={(e)=>deleteRegister(register._id,e)}>Delete</button>
                                         </td>
                                         <td>
-                                            <UpdateModalComponent id={post._id} name={post.name}date={post.date}email={post.email}cel={post.cel}rg={post.rg}cpf={post.cpf}address={post.address}/>
+                                            <UpdateModalComponent id={register._id} 
+                                            name={register.name}
+                                            date={register.date}
+                                            email={register.email}
+                                            cel={register.cel}
+                                            rg={register.rg}
+                                            cpf={register.cpf}
+                                            address={register.address}
+                                            city={register.city}
+                                            state={register.state}
+                                            zip={register.zip}/>
                                         </td>
                                     </tr>
                                 )
